7f9857e53858ecd8f8be1c1ba48df767f07fe8b0
alumno <alumno@10.0.2.5>
2020-04-25 23:19:27 +0200

    Commit Ficheros Proyecto

---
+++ src/gestionvideoclub/GestionVideoclub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionvideoclub;
+
+/**
+ *
+ * @author alumno
+ */
+public class GestionVideoclub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
