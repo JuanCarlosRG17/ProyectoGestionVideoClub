3cea34af5366083e38155aad36164fabc843f1b0
alumno <alumno@10.0.2.5>
2020-04-26 23:22:50 +0200

    Commit Ficheros Proyecto 3

--- src/gestionvideoclub/GestionVideoclub.java
+++ src/gestionvideoclub/GestionVideoclub.java
@@ -5,6 +5,8 @@
  */
 package gestionvideoclub;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author alumno
@@ -16,6 +18,21 @@ public class GestionVideoclub {
      */
     public static void main(String[] args) {
         
+        ArrayList <Pelicula> peli = new ArrayList<Pelicula>();
+		
+	peli.add(new Pelicula("Avengers","Charly","4545G"));
+	peli.add(new Pelicula("Sonic","Jimmy","1212A"));
+	peli.add(new Pelicula("Mulan","Xiang","7878W"));
+	peli.add(new Pelicula("El Rey Leon","Dinsey","6565T"));
+	peli.add(new Pelicula("Aladin","Mahoma","9595B"));
+	peli.add(new Pelicula("Star Wars","Pepe","9999H"));
+	peli.add(new Pelicula("Stand By Me Doraemon","Mitsubisi","3333Ñ"));
+	peli.add(new Pelicula("Tintanic","Juanjo","7777V"));
+	peli.add(new Pelicula("Joker","El Risas","1111L"));
+	peli.add(new Pelicula("Batman","Bruce","8686P"));
+		
+		 
+	ArrayList <Cliente> cli = new ArrayList<Cliente>();
     }
     
 }
